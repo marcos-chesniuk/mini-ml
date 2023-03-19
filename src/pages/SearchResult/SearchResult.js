f1990b15adf60755dd39d129efe97544e72ccbe9
Juan Marcos Chesniuk <jmdchesniuk@gmail.com>
2023-03-19 03:25:15 -0300

    Added categories section

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import getProducts from "../../services/Products";
+import getCategories from "../../services/Categories";
 import { useLoaderData } from 'react-router';
 
 export async function loader ({ request, params }) {
     const url = new URL(request.url);
     const searchTerm = url.searchParams.get('search');
     const products = await getProducts(searchTerm)
-    return {products}
+    const categories = await getCategories(searchTerm) || ['No se pudieron obtener las categorías']
+    return {products, categories}
 }
 const SearchResult = (props) => {
-    const {products} = useLoaderData()
+    const {products, categories} = useLoaderData()
     console.log(products)
+
+    function formatCategories() {
+        return <div>
+            {
+                categories.map((category) => category.name)
+                .map((value, index, arr) => {
+                    if (index === arr.length - 1) {
+                        return <strong key={value} >{ value }</strong>
+                    } else {
+                        return <span key={value} >{ value + ' > '} </span>
+                    }
+                })
+            }
+        </div>
+    }
 
     return (
         <div>
-            <h1>RESULTS</h1>
+            {formatCategories()}
             { products &&
                 <ul>
                     {products.map((item) => (
--- src/services/Products.js
+++ src/services/Products.js
@@ -2,6 +2,7 @@ export default function getProducts(searchTerm) {
     return fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${searchTerm}&limit=4`)
     .then(res => res.json())
     .then(response => {
+        console.log(response)
         return response.results
     })
 }
